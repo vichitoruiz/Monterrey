79eb18157a479535a7c4bae1064c7f333b39e45c
vichitoruiz <vichitoruiz@hotmail.com>
2025-11-11 21:28:09 -0500

    Eliminar windows form
    se eliminó windows form proyecto, ya no se trabaja con winfor

--- SSEL.MONTERREY.WinForms/Forms/FrmDashboard.cs
+++
@@ -1,9 +0,0 @@
-using System.Windows.Forms;
-
-namespace SSEL.MONTERREY.WinForms.Forms;
-public class FrmDashboard : Form {
-  public FrmDashboard(){
-    Text = "SSEL MONTERREY - Dashboard";
-    Width = 1200; Height = 720;
-  }
-}
--- SSEL.MONTERREY.WinForms/Forms/FrmLogin.cs
+++
@@ -1,6 +0,0 @@
-using System.Windows.Forms;
-
-namespace SSEL.MONTERREY.WinForms.Forms;
-public class FrmLogin : Form {
-  public FrmLogin(){ Text = "Login"; Width = 420; Height = 320; }
-}
